b1b9da225db9933e34d1f0aeaf58c6c2480187ae
Luan Louzada <luanxdd@gmail.com>
2022-10-19 02:26:22 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import "../styles/global.scss";
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <NextAuthProvider session={session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
+import { SignInButton } from "../../../components/SignInButton";
+import { userAgent } from "next/server";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      authorization: {
+        params: { scope: "user" },
+      },
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      console.log(user);
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+  },
+};
+export default NextAuth(authOptions);
